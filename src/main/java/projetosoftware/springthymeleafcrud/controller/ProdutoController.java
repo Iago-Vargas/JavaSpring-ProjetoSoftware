f3488a3ef1a65facf9a149d974467f657254c0da
Iago Vargas <iro.6151@gmail.com>
2025-05-12 10:23:44 -0300

    Att 2

--- src/main/java/projetosoftware/springthymeleafcrud/controller/ProdutoController.java
+++ src/main/java/projetosoftware/springthymeleafcrud/controller/ProdutoController.java
@@ -21,14 +21,34 @@ public class ProdutoController {
         return "form";
     }
     @PostMapping("/salvar")
-    public @ResponseBody String salvar(@ModelAttribute Produto produto){
+    public String salvar(@ModelAttribute Produto produto){
         produtoRepository.save(produto);
-        return "Salvo!";
+        return "redirect:/produto/listar";
     }
 
     @GetMapping("/listar")
     public String listar(Model model) {
         model.addAttribute("produtos", produtoRepository.findAll()); //findAll retorna todos itens
         return "Lista";
+    }
+
+    //Método para editar o produto, recebendo id do produto
+    @GetMapping("/editar/{id}")
+    public String formEditar(@PathVariable int id, Model model){
+        //Criamos um novo objeto do tipo produto com informações do banco e do id passado
+        Produto produto = produtoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Produto invalido:"+id));
+        // Adicionamos o objeto produto criado ao modelo (pagina html)
+        model.addAttribute("produto", produto);
+        return "form";
+    }
+
+    //Metodo para deletar o produto
+    @GetMapping("/deletar/{id}")
+    public String deletar(@PathVariable int id){
+        //Criamos um novo objeto do tipo produto com informações do banco e do id passado
+        Produto produto = produtoRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Produto invalido:"+id));
+        // Adicionamos o objeto produto criado ao modelo (pagina html)
+        produtoRepository.delete(produto);
+        return "redirect:/produto/listar";
     }
 }
